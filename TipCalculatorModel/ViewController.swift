eb8b2c9c6673730f05bd56c8e830e9b07722c7b8
basxeon <supachaixeon@gmail.com>
2015-02-06 17:34:16 +0700

    Tipcalculator

--- TipCalculatorModel/ViewController.swift
+++ TipCalculatorModel/ViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var totalTextField: UITextField!
+    @IBOutlet weak var taxPactlabel: UILabel!
+    @IBOutlet weak var resultTextview: UITextView!
+    @IBOutlet weak var taxPactslider: UISlider!
 
+    
+
+    @IBAction func calculateTapped(sender: AnyObject){
+        
+        tipCalc.total = Double((totalTextField.text as NSString).doubleValue)
+        
+        let possibleTips = tipCalc.returnPossibleTips()
+        var result = ""
+        
+        var key = Array(possibleTips.keys)
+        sort(&key)
+        
+        for (tipPct, tipValue) in possibleTips {
+            let tipValue = possibleTips[tipPct]!
+            let prettyTipValue = String(format: "%.2f", tipValue )
+            result += "\(tipPct)%: \(prettyTipValue)\n"
+        }
+        resultTextview.text = result
+    }
+    
+    @IBAction func taxPercentageChanged(sender: AnyObject) {
+        tipCalc.taxPct = Double(taxPactslider.value)/100.0
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender: AnyObject) {
+        totalTextField.resignFirstResponder()
+    }
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI(){
+        
+        totalTextField.text = String(format: "%0.2f",tipCalc.total)
+        
+        taxPactslider.value = Float(tipCalc.taxPct) * 100.0
+        
+        taxPactlabel.text = "Tax Percentage (\(Int(taxPactslider.value))%)"
+        resultTextview.text = ""
+    
+    }
 
 }
 
